4b007677418f55238d8f5a979364071939427f38
aidil2049l <lidgref@gmail.com>
2023-07-28 21:37:19 +0200

    yes

---
+++ azure-app-service/startup.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+# cp /home/site/nginx.conf /etc/nginx/sites-available/default
+# or -if you are storing the nginx.conf in your Git repository-:
+cp /home/site/repository/azure-app-service/nginx.conf /etc/nginx/sites-available/default
+
+service nginx reload
